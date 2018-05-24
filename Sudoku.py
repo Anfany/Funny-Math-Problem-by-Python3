eb8f3037b2e2abdd00cbd2436d3c785e27fb913e
Anfany <36292421+Anfany@users.noreply.github.com>
2018-05-24 16:41:23 +0800

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -1,62 +1,62 @@
+#  因引入库
 import pandas as pd
 import numpy as np
 import os
  
-#数据文件路径
-DataPath=r'C:\Users\GWT9\Desktop\Pro1 数独Sudoku'
+# 存储数独题目的Excel文件路径
+DataPath = r'C:\Users\AnFany\Desktop\Sudoku'
 os.chdir(DataPath)
-#文件名
-FileName='Sudoku.xlsx'
-#规格
-sukoducount=9
-#读取数据
-ReadData=pd.read_excel(r'./%s'%FileName)
+# 文件名
+FileName = 'Sudoku.xlsx'
+# 宫数设置，根据题目宫数的不同可选择9,16,25(数字越大，解题速度越慢)
+sukoducount = 9
+# 读取文件
+ReadData = pd.read_excel(r'./%s'%FileName)
+# 数独题目
+NumData = ReadData.values
+# 打印数独题目
+print('号称最难的数独题目:\n', NumData)
 
-#数据
-NumData=ReadData.values
+#  判断数独题目的完整性
+while len(NumData) < sukoducount:
+    NumData = np.row_stack((NumData, [np.nan] * sukoducount))
 
-print('号称最难的数独题目:\n',NumData)
-
-#判断数据行的完整性
-while len(NumData)<sukoducount:
-    NumData=np.row_stack((NumData,[np.nan]*sukoducount))
-
-#计算可能性
+#  根据格子的位置，计算该位置可以填写的数字集合
 def ProbNumber(hang,lie,data):
-    #行数据
-    H=list(data[hang])
-    #列数据
-    L=list(data[:,lie])
-    #宫数据
-    G=[]
-    sfang=int(len(data)**0.5)
-    hh=hang//sfang
-    ll=lie//sfang
-    #宫数据添加
+    # 行数据
+    H = list(data[hang])
+    # 列数据
+    L = list(data[:, lie])
+    # 宫数据
+    G = []
+    sfang = int(len(data) ** 0.5)
+    hh = hang // sfang
+    ll = lie // sfang
     for ig in range(sfang):
         for gi in range(sfang):
-            G.append(data[hh*sfang+ig,ll*sfang+gi])
-    #该点的可能性
+            G.append(data[hh * sfang + ig, ll * sfang + gi])
+    # 行，列，宫已经包含的数字集合  
     lal=list(H)+list(L)+G
-    prob=[ip for ip in list(range(1,len(data)+1)) if ip not\
-          in lal]
+    # 该空格可能选择的数字集合
+    prob=[ip for ip in list(range(1,len(data)+1)) if ip not in lal]
     return prob
 
 
-#为数据中的空元素选择可能性字典
+# 为题目中的空格计算填写数字的可能性，输出字典形式
 def ForK(data):
-    Kdict={}
+    Kdict = {}
     for ik in range(len(data)):
         for ki in range(len(data)):
-            if np.isnan(data[ik,ki]):
-                #转换，便于字典的最下值是唯一的
-                trans=ProbNumber(ik,ki,data)
-                jieti=len(trans)*10000000+ik*10000+ki*10
-                Kdict['%s-%s'%(ik,ki)]=[jieti,len(trans),trans]
+            if np.isnan(data[ik, ki]): # 判断空格
+                # 计算可填写数字的集合
+                trans = ProbNumber(ik, ki, data)
+                # 转换是为了让所有空格的jieti值中，只有唯一的一个最小值
+                jieti = len(trans) * 10000000 + ik * 10000 + ki * 10
+                Kdict['%s-%s'%(ik, ki)] = [jieti, len(trans), trans]
     return Kdict
 
 
-#选择可能性最小的位置
+#  选择可能性最小的位置
 def SeleM(ddict):
     Small=min(ddict.items(),key=lambda x:(x[1][0]))[0]
     #位置
@@ -142,6 +142,8 @@ import time
 start=time.clock()
 Sudoku()
 print('用时%s秒'%(time.clock() - start))
+
+
 
 
 
