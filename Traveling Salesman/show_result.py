f92180c3633831eb9600e19d88012f7bda840086
Anfany <36292421+Anfany@users.noreply.github.com>
2019-04-17 10:52:03 +0800

    Update show_result.py

--- Traveling Salesman/show_result.py
+++ Traveling Salesman/show_result.py
@@ -10,34 +10,34 @@ from pyecharts_snapshot.main import make_a_snapshot
 
 style = Style(
     title_pos="center",
-    width=410,
-    height=280,
+    width=600,
+    height=480,
     background_color="#d8e3e7",
-    title_text_size=12,
-    subtitle_text_size=9,
+    title_text_size=15,
+    subtitle_text_size=12,
     title_color='#134567',
-    subtitle_color='#134567'
+    subtitle_color='#134567',
 )
 
 style_geo = style.add(
     is_label_show=True,
     label_formatter="{b}",
     is_legend_show=True,
-    label_text_size=6,
-    label_text_color='#c02c38',
-    line_width=1,
+    label_text_size=8,
+    label_text_color='#000000',
+    line_width=2,
     label_pos="inside",
-    label_color=['#000000', '#e2e1e4'],
+    label_color=['#2474b5', '#e26674'],
     geo_effect_symbolsize=0,
-    line_opacity=0.6,
+    line_opacity=1,
     geo_normal_color="#e2e1e4",
-    symbol_size=0,
+    symbol_size=10,
     border_color='#7a7374',
 )
 
 
 def create_charts(data_path, path_html, path_png, length):
-    charts = GeoLines("TSP问题：以全国34市百度地图驾车距离为例", '路径：%.3fkm' % (length / 1000), **style.init_style)
+    charts = GeoLines("TSP问题：全国34城市为例", '路径：%.3fkm' % (length / 1000), **style.init_style)
     charts.add('', data_path, **style_geo)
     charts.render(path_html)
     make_a_snapshot(path_html, path_png)
